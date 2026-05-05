d822d358b87ad766b1e6f6bfbbe1e8b11c71203a
Djepatalem Renaud <djepatalemrenaud@gmail.com>
2026-05-05 15:33:59 +0100

    mise à jour

---
+++ triple.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int triple(int nombre)
+{
+	int resultat = 0;
+
+	resultat = 3 * nombre;
+	return resultat;
+}
+int main()
+{
+	printf("%d \n", triple(-2));
+	return 0;
+	printf("\n");
+}
