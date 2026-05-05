471dde1e32449f5f0bfec05636fbdac66ff55919
Djepatalem Renaud <djepatalemrenaud@gmail.com>
2026-05-05 12:51:35 +0100

    Affiche avec la boucle While

---
+++ affiche.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int compteur = 1;
+	while (compteur < 10)
+	{
+		printf("La variable du compteur vaut %d\n", compteur);
+		compteur++;
+	}
+
+	printf("\n");
+	return 0;
+}
---
+++ nomb47.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int nombEntr = 0;
+
+	while ( nombEntr != 47 )
+	{
+		printf("Entre le nombre 47 ! ");
+		scanf("%d", &nombEntr);
+	}
+
+	printf("Bravo vous avez entré 47 \n");
+	return 0;
+}
+
+
+
